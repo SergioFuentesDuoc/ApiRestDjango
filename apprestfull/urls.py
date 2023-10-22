68f0fc4f346adeca08c2abe15e6a74bd8633dac8
sfuentes <serg.fuentes@profesor.duoc.cl>
2023-10-22 19:18:54 -0300

    Se agrega urls.py usuario

--- apprestfull/urls.py
+++ apprestfull/urls.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
-from .api   import ProductoViewSet
+from .api   import ProductoViewSet, UsuarioViewSet
 
 router = routers.DefaultRouter()
 
 router.register('api/productos', ProductoViewSet, 'productos')
+router.register('api/usuarios', UsuarioViewSet, 'usuarios')
 
 urlpatterns = router.urls
