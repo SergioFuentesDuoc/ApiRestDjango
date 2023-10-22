ee14ae0e2143d7d9ff7e91e76bc152225ed67cbf
sfuentes <serg.fuentes@profesor.duoc.cl>
2023-10-22 19:04:35 -0300

    Se agrega api.py usuario

--- apprestfull/api.py
+++ apprestfull/api.py
@@ -1,8 +1,14 @@
-from .models import Producto
+from .models import Producto, Usuario
 from rest_framework import viewsets, permissions
-from .serializers import ProductoSerializer
+from .serializers import ProductoSerializer, UsuarioSerializer
 
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = ProductoSerializer
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UsuarioSerializer
+
