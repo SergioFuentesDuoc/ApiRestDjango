6541b03ebe8c218bbda5353e0828ae63c4debf61
sfuentes <serg.fuentes@profesor.duoc.cl>
2023-10-22 19:43:19 -0300

    modifica api.py

--- apprestfull/urls.py
+++ apprestfull/urls.py
@@ -1,9 +1,18 @@
+from django.urls import path, include
 from rest_framework import routers
 from .api   import ProductoViewSet, UsuarioViewSet
 
 router = routers.DefaultRouter()
 
-router.register('api/productos', ProductoViewSet, 'productos')
-router.register('api/usuarios', UsuarioViewSet, 'usuarios')
+#router.register('api/productos', ProductoViewSet, 'productos')
+#router.register('api/usuarios', UsuarioViewSet, 'usuarios')
 
-urlpatterns = router.urls
+#urlpatterns = router.urls
+
+router.register('api/productos', ProductoViewSet, 'productos',basename="productos")
+router.register('api/usuarios', UsuarioViewSet, 'usuarios',basename="usuarios")
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
