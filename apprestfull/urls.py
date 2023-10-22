aa3262ab7bda1014749fe27cc8e009cee4cf31b3
sfuentes <serg.fuentes@profesor.duoc.cl>
2023-10-22 20:33:43 -0300

    Se agrega urls.py usuario y productos

--- apprestfull/urls.py
+++ apprestfull/urls.py
@@ -1,9 +1,19 @@
+from django.urls import path, include
 from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 from .api   import ProductoViewSet, UsuarioViewSet
 
-router = routers.DefaultRouter()
+# Crea un enrutador de DRF
+router = DefaultRouter()
+router.register(r'productos', ProductoViewSet)
+router.register(r'usuarios', UsuarioViewSet)
 
-router.register('api/productos', ProductoViewSet, 'productos')
+urlpatterns = [
+    # Rutas generadas automáticamente por el enrutador de DRF
+    path('api/', include(router.urls)),
+]
 
-
-urlpatterns = router.urls
+# También puedes agregar rutas personalizadas si es necesario
+# urlpatterns += [
+#     path('otra_ruta/', MiVistaPersonalizada.as_view(), name='nombre_ruta_personalizada'),
+# ]
